cd4583934cc93ea0444be890fe982b5401f95dfa
Mohit <69758819+mohitwasnik132@users.noreply.github.com>
2022-02-24 06:02:58 +0000

    String-quotes

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-#Write your code below this line 👇
+print ("hello mohit")
 
 
+print ("Day 1 - Python Print Function")
+
+print ("The function is declared like this:")
+
+print ("print('what to print')")
 
 
+print ('"This is same as double quotes"')
 
-
-
-
-
+#To use quotes in a quotes
+#keep inner quotes different from outer
 
