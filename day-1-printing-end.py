e8c3a62e35406b30ea592dc01cb00129914b833b
mohitwasnik132 <69758819+mohitwasnik132@users.noreply.github.com>
2022-03-02 10:13:57 +0530

    Update day-1-printing-end.py

--- day-1-printing-end.py
+++ day-1-printing-end.py
@@ -4,5 +4,10 @@
 # input() will get user input in console
 # Then print() will print the word "Hello" and the user input
 print("Hello " + input("What is your name?"))
-```
 
+a = int(input("number1"))
+b = 2
+c = a + b
+print ("The addition of a+b is C =  ", c)
+
+```
